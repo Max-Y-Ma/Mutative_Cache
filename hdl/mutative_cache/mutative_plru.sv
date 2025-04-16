6622a9c6f5f6222864d38bb3c897ec455ec2bfe3
anishks2 <anishks2@gelib-4c-39.ews.illinois.edu>
2025-04-15 22:46:42 -0500

    plru explictly set

--- hdl/mutative_cache/mutative_plru.sv
+++ hdl/mutative_cache/mutative_plru.sv
@@ -11,13 +11,121 @@ import mutative_types::*;
 
 );
     logic [WAYS-2:0] PLRU_bits[SET_SIZE]; 
-    logic [WAYS-2:0] PLRU_bits_next;
-    logic [WAYS-2:0] PLRU_update_mask;
-    logic [WAY_IDX_BITS-1:0] hit_way_inv;
-    assign hit_way_inv = ~hit_way;
+    // logic [WAYS-2:0] PLRU_bits_next;
+    // logic [WAYS-2:0] PLRU_update_mask;
+    // logic [WAY_IDX_BITS-1:0] hit_way_inv;
+    // assign hit_way_inv = ~hit_way;
 
 
-    always_comb begin
+    always_ff @(posedge clk)begin: plru_1 //TODO: FIX MAYBE MAKE $ OF THEM
+        if(rst) begin
+            for(int unsigned i = 0; i < SET_SIZE; i++) begin
+                PLRU_bits[i] <= {(WAYS-1){1'b0}};
+            end
+        end
+        else if(hit)begin
+            if (setup == 2'b01) begin
+                PLRU_bits[cache_address.set_index] <= hit_way;
+            end
+            else if (setup == 2'b10) begin
+                case (hit_way)
+                    2'd0: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b00};
+                    2'd1: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b10};
+                    2'd2: PLRU_bits[cache_address.set_index] <= {1'b0, PLRU_bits[cache_address.set_index][1], 1'b1};
+                    2'd3: PLRU_bits[cache_address.set_index] <= {1'b1, PLRU_bits[cache_address.set_index][1], 1'b1};
+                endcase
+            end else if (setup == 2'b11) begin
+                case (hit_way)
+                    3'd0: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][6:4], 1'b0, PLRU_bits[cache_address.set_index][2], 2'b00};
+                    3'd1: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][6:4], 1'b1, PLRU_bits[cache_address.set_index][2], 2'b00};
+                    3'd2: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][6:5], 1'b0, PLRU_bits[cache_address.set_index][3:2], 2'b10};
+                    3'd3: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][6:5], 1'b1, PLRU_bits[cache_address.set_index][3:2], 2'b10};
+                    3'd4: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][6], 1'b0, PLRU_bits[cache_address.set_index][4:3], 1'b0, PLRU_bits[cache_address.set_index][1], 1'b1};
+                    3'd5: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][6], 1'b1, PLRU_bits[cache_address.set_index][4:3], 1'b0, PLRU_bits[cache_address.set_index][1], 1'b1};
+                    3'd6: PLRU_bits[cache_address.set_index] <= {1'b0, PLRU_bits[cache_address.set_index][5:3], 1'b1, PLRU_bits[cache_address.set_index][1], 1'b1};
+                    3'd7: PLRU_bits[cache_address.set_index] <= {1'b1, PLRU_bits[cache_address.set_index][5:3], 1'b1, PLRU_bits[cache_address.set_index][1], 1'b1};
+                endcase
+            end
+        end
+    end
+ 
+ 
+
+    always_comb begin : replacement_1 //TODO: MAKE 4 and FIX
+        case (setup)
+            2'b01: begin
+                case (~PLRU_bits[cache_address.set_index]) 
+                    1'b0: begin
+                        evict_way = '0;
+                        evict_we = 1 << evict_way;
+                    end
+                    1'b1: begin
+                        evict_way = '1;
+                        evict_we = 1 << evict_way;
+                    end
+                endcase
+            end
+            2'b10: begin
+                casez (~PLRU_bits[cache_address.set_index])
+                    3'b?00: begin
+                        evict_we = 4'b0001;//replace way 0
+                        evict_way = 2'b00;
+                    end
+                    3'b?10: begin
+                        evict_we = 4'b0010;//replace way 1
+                        evict_way = 2'b01;
+                    end
+                    3'b0?1: begin 
+                        evict_we = 4'b0100;//replace way 2
+                        evict_way = 2'b10;
+                    end
+                    3'b1?1: begin 
+                        evict_we = 4'b1000;//replace way 3
+                        evict_way = 2'b11;
+                    end
+                endcase
+            end
+            2'b11: begin
+                casez (~PLRU_bits[cache_address.set_index])
+                    7'b???0?00: begin
+                        evict_way = '0;
+                        evict_we = 1 << evict_way;
+                    end
+                    7'b???1?00: begin
+                        evict_way = 3'b001;
+                        evict_we = 1 << evict_way;
+                    end
+                    7'b??0??00: begin
+                        evict_way = 3'b010;
+                        evict_we = 1 << evict_way;
+                    end
+                    7'b??1??00: begin
+                        evict_way = 3'b011;
+                        evict_we = 1 << evict_way;
+                    end
+                    7'b?0??0?1: begin
+                        evict_way = 3'b100;
+                        evict_we = 1 << evict_way;
+                    end
+                    7'b?1??0?1: begin
+                        evict_way = 3'b101;
+                        evict_we = 1 << evict_way;
+                    end
+                    7'b0???1?1: begin
+                        evict_way = 3'b110;
+                        evict_we = 1 << evict_way;
+                    end
+                    7'b1???1?1: begin
+                        evict_way = 3'b111;
+                        evict_we = 1 << evict_way;
+                    end
+                endcase
+            end
+        endcase
+    end
+
+
+    /*always_comb begin
         PLRU_bits_next    = '0;
         PLRU_update_mask  = '0;
 
@@ -56,7 +164,7 @@ import mutative_types::*;
                 end
             end
         end
-    end 
+    end */
 
     /*always_comb begin
         PLRU_bits_next = '0;
@@ -82,31 +190,31 @@ import mutative_types::*;
 
 
 
-    always_ff @(posedge clk)begin: plru_1 
-        if(rst) begin
-            for(int unsigned i = 0; i < SET_SIZE; i++) begin
-                PLRU_bits[i] <= {(WAYS-1){1'b0}};
-            end
-        end
-        else if(hit)begin
-            PLRU_bits[cache_address.set_index] <= (PLRU_bits[cache_address.set_index] & ~PLRU_update_mask) | (PLRU_bits_next & PLRU_update_mask);
+    // always_ff @(posedge clk)begin: plru_1 
+    //     if(rst) begin
+    //         for(int unsigned i = 0; i < SET_SIZE; i++) begin
+    //             PLRU_bits[i] <= {(WAYS-1){1'b0}};
+    //         end
+    //     end
+    //     else if(hit)begin
+    //         PLRU_bits[cache_address.set_index] <= (PLRU_bits[cache_address.set_index] & ~PLRU_update_mask) | (PLRU_bits_next & PLRU_update_mask);
 
-            /*for(int i = 0; i < WAYS; i++) begin
-                if(hit_way == i) begin
-                    PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b00};
-                end
-            end
+    //         /*for(int i = 0; i < WAYS; i++) begin
+    //             if(hit_way == i) begin
+    //                 PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b00};
+    //             end
+    //         end
 
-            unique case (hit_way)
-                2'd0: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b00};
-                2'd1: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b10};
-                2'd2: PLRU_bits[cache_address.set_index] <= {1'b0, PLRU_bits[cache_address.set_index][1], 1'b1};
-                2'd3: PLRU_bits[cache_address.set_index] <= {1'b1, PLRU_bits[cache_address.set_index][1], 1'b1};
-            endcase*/
-        end
-    end
+    //         unique case (hit_way)
+    //             2'd0: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b00};
+    //             2'd1: PLRU_bits[cache_address.set_index] <= {PLRU_bits[cache_address.set_index][2], 2'b10};
+    //             2'd2: PLRU_bits[cache_address.set_index] <= {1'b0, PLRU_bits[cache_address.set_index][1], 1'b1};
+    //             2'd3: PLRU_bits[cache_address.set_index] <= {1'b1, PLRU_bits[cache_address.set_index][1], 1'b1};
+    //         endcase*/
+    //     end
+    // end 
 
-    always_comb begin
+    /*always_comb begin
         evict_way = '0;
         evict_we  = '0;
 
@@ -137,7 +245,7 @@ import mutative_types::*;
         end
 
         evict_we = 1 << evict_way;
-    end
+    end */
 
 
     /*always_comb begin : replacement_1 
